c2b6018f7d38009a489b0a28c68bce37f9bd91f2
zgt <2506526350@qq.com>
2016-10-09 17:38:49 +0800

    add annotate gem

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'semantic-ui-sass', '~> 2.2', '>= 2.2.4.0'
+  gem 'annotate', '~> 2.7', '>= 2.7.1'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  number     :string(255)
+#  amount     :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Book < ApplicationRecord
 end
