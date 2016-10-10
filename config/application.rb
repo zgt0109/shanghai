38079e778ad5c47077a7106ab3262876e2d8e1a9
zgt <2506526350@qq.com>
2016-10-10 11:02:05 +0800

    修改new edit show destroy 中文显示

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Shanghai
   class Application < Rails::Application
   	config.generators.assets = false
   	config.generators.helper = false
+  	config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+		config.i18n.available_locales = ['zh-CN', :en]
+		config.i18n.default_locale = 'zh-CN'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
